f98bd5b3e63e79020b64bf7c1dc18a5f5e488c3f
Muli4 <mulimaurice730@gmail.com>
2025-02-16 14:59:11 +0300

    js

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+function toggleForm() {
+    let loginForm = document.getElementById("login-form");
+    let registerForm = document.getElementById("register-form");
+    let title = document.getElementById("form-title");
+
+    if (loginForm.style.display === "none") {
+        loginForm.style.display = "block";
+        registerForm.style.display = "none";
+        title.innerText = "Login";
+    } else {
+        loginForm.style.display = "none";
+        registerForm.style.display = "block";
+        title.innerText = "Register";
+    }
+    loginForm.style.opacity = 0;
+    registerForm.style.opacity = 0;
+    setTimeout(()=>{
+        loginForm.style.opacity = 1;
+        registerForm.style.opacity = 1;
+    }, 2000);
+}
