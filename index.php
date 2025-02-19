6225e77768b9a80b01d6f43861bfaf931639689a
Muli4 <mulimaurice730@gmail.com>
2025-02-19 16:50:11 +0300

    added index.php

--- index.php
+++ index.php
@@ -1,11 +1,44 @@
+<?php
+
+include './db.php';
+session_start();
+
+// select the available cars
+
+$sql = "SELECT * FROM cars WHERE status = 'available'";
+$result = $conn->query($sql);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>index</title>
 </head>
 <body>
-    <a href="./pages/auth.php">authentication</a>
+    <header>
+        <nav>
+            <?php if(isset($_SESSION['user_id'])):?>
+                <a href="rentals.php">My rentals</a>
+                <a href="logout.php">logout</a>
+            <?php else: ?>
+                <a href="./pages/auth.php"></a>
+            <?php endif ; ?>
+        </nav>
+    </header>
+
+    <section class="cars">
+        <?php while($car = $result->fetch_assoc()): ?>
+            <div class="car-card">
+                <img src="images/<?php echo $car['image']; ?>" alt="<?php echo $car['name']; ?>">
+                <h3><?php echo $car["name"] ;?></h3>
+                <p><?php echo $car["description"]; ?></p>
+                <p><strong>Price:$<?php echo $car["price"];?>/day</strong></p>
+                <a href="<?php echo isset($_SESSION["user_id"]) ? 'rent.php?id='.$car['id'] : 'auth.php'; ?>" class="rent-btn">Rent Now</a>
+            </div>
+        <?php endwhile; ?>
+    </section>
 </body>
 </html>
