b081d8a401a31b4722d8465e5c7bf183d63060a1
Muli4 <mulimaurice730@gmail.com>
2025-03-07 12:52:46 +0300

    new

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function toggleForm() {
+    document.getElementById("login-form").style.display =
+        document.getElementById("login-form").style.display === "none" ? "block" : "none";
+    document.getElementById("register-form").style.display =
+        document.getElementById("register-form").style.display === "none" ? "block" : "none";
+}
+
+
